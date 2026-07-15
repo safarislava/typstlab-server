acf3cea086b5fedfa2beceb5915d26f36664c9df
safarislava <safarislava@gmail.com>
2026-07-15 15:33:05 +0300

    impl: Add CI with strong rules and start application

---
+++ main.go
@@ -0,0 +1,27 @@
+// Package main is the entry point for the TypstLab Server application.
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	fmt.Println("Starting TypstLab Server...")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("OK"))
+	})
+}
+
+func Add(a, b int) int {
+	return a + b
+}
