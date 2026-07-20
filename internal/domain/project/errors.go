0aa7fd46fe64cdfd58d1654135af2a61677855cc
safarislava <safarislava@gmail.com>
2026-07-20 15:56:13 +0300

    fix: Remove unused definition

--- internal/domain/project/errors.go
+++ internal/domain/project/errors.go
@@ -8,5 +8,4 @@ var (
 	ErrEmptyUpdatedAt = errors.New("project updatedAt cannot be empty")
 	ErrEmptyUserID    = errors.New("project user id cannot be empty")
 	ErrNoUsers        = errors.New("project must have at least one user")
-	ErrEmptyFileID    = errors.New("file id cannot be empty")
 )
