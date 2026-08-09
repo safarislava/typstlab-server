75c191c71d5bf09fa4edff4678b285c93ca44c1a
safarislava <safarislava@gmail.com>
2026-08-10 00:07:55 +0300

    feat: Adds the initial database schema.

---
+++ migrations/000001_init_schema.down.sql
@@ -0,0 +1,7 @@
+-- 000001_init_schema.down.sql
+
+DROP TABLE IF EXISTS files;
+DROP TABLE IF EXISTS project_members;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS sessions;
+DROP TABLE IF EXISTS users;
